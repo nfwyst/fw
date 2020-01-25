630b4867e7e0843977c61a012eab9700ff6c9e8b
nfwyst <nfwyst@gmail.com>
2020-01-25 21:28:26 +0800

    nfwyst@build collection stuff

--- src/test/index.ts
+++ src/test/index.ts
@@ -1,10 +1,27 @@
-import { User } from './User'
-import { UserEdit } from './UserEdit'
+// import { User } from './User'
+// import { UserEdit } from './UserEdit'
 
-const user = User.buildUser({ username: 'bob', age: 40 })
+// const user = User.buildUser({ username: 'bob', age: 40 })
 
-const root = document.getElementById('root')
-if (!root) throw new Error('root not found')
+// const root = document.getElementById('root')
+// if (!root) throw new Error('root not found')
 
-const editor = new UserEdit(root, user)
-editor.render()
+// const editor = new UserEdit(root, user)
+// editor.render()
+
+import { UserList } from './UserList'
+import { Collection } from '../models/Collection'
+import { User, UserProps } from '../test/User'
+
+const users = new Collection('http://localhost:3000/users', (json: UserProps) => {
+  return User.buildUser(json)
+})
+
+users.on('change', () => {
+  const root = document.getElementById('root')
+  if (root) {
+    new UserList(root, users).render()
+  }
+})
+
+users.fetch()
